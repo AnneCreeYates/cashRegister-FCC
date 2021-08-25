4636c7958eaaf92200cb9bc27c3d3e3a4b237f90
AnneC.Yates <annecreeyates@gmail.com>
2021-08-25 23:08:16 +0100

    Adds working iteration through array - resulting in all the relevant numbers being pushed to cidSummedUp

--- main.js
+++ main.js
@@ -14,13 +14,25 @@ function checkCashRegister(price, cash, cid) {
 
   let cidSummedUp = [];
 
-  cid.forEach(arr => {
-    arr.forEach(item => console.log(item));
-  })
-  
+  /*Trying forEach option - so far not working well*/
+  // cid.forEach(arr => {
+  //   arr.forEach(item => console.log(item));
+  // })
+  /*double iteration through arrays - prints all the numbers twice*/
   // for (let i=0; i<cid.length; i++){
-    // cidSummedUp.push(cid[0][1]);
+  //   for (let j=0; j<cid[i].length; j++){
+  //     cidSummedUp.push(cid[i][1]);
+  //   }
+    
   // };
+
+  let cidSummedUp = [];
+   for (let i=0; i<cid.length; i++){
+     cidSummedUp.push(cid[i][1]);
+    }
+  
+   console.log(cidSummedUp);
+  };
   
 
     // if( change < cidSummedUp){
@@ -31,7 +43,7 @@ function checkCashRegister(price, cash, cid) {
 
     // }
 
-  }
+  
   
 
   checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
